3b00bc378de7300fe2660602cbe4cce2e712779b
emdads <emidiasonline@gmail.com>
2025-09-05 17:27:18 -0300

    Update ProtectedRoute.tsx

--- src/react-app/components/ProtectedRoute.tsx
+++ src/react-app/components/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import { useAuth } from '../hooks/useAuth';
-import { Navigate } from 'react-router';
+import { Navigate } from 'react-router-dom';
 import { Shield, AlertCircle } from 'lucide-react';
 
 interface ProtectedRouteProps {
